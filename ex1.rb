fde4f3eaadcdcddf7c15a49fa8e87c85c1ebe39f
tai <tai.kokkhuntod@gmail.com>
2013-05-29 11:08:08 +0700

    Ex.3

--- ex1.rb
+++ ex1.rb
@@ -1,7 +1,14 @@
 # Ex.1 
 animals = ['Cat','Dog','Iguana',nil]
 #print animals
-
+#Ex2
 	animals.each do |animals|
 		puts animals
 	end 
+#Ex	3
+
+personal = { 
+  name: "Tai",
+  email: "taik@revenuescrien.com",
+  colors: ["blue", "red", "black"]
+}
