287c06bf26a786e18810e68a0e8fcff397b12787
tai <tai.kokkhuntod@gmail.com>
2013-05-29 11:20:00 +0700

    Ex.4

--- ex1.rb
+++ ex1.rb
@@ -12,3 +12,17 @@ personal = {
   email: "taik@revenuescrien.com",
   colors: ["blue", "red", "black"]
 }
+#Ex 4
+class Animals
+  def initialize(name,age,sex)
+    @name = name 
+    @age = age
+    @sex = sex
+  end
+
+  attr_accessor:name
+  attr_accessor:age
+  attr_accessor:sex
+end
+	 test = Animals.new("tai","31","m")
+ 	 print test.name + " age: " + test.age + " sex: " + test.sex
